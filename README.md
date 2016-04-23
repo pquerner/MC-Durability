bb76d7b7cf3b65c6994b7175ec6d7599c7ba83b7
Pascal Querner <hello@paskl.de>
2016-04-23 21:59:40 +0200

    added hoe to durability select

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Version: v0.1
 
 ## What does it do?
 
-This plugin will warn you if you get 20 or less durability on any craftsmen item (axe, pickaxe, shovel, sword).
+This plugin will warn you if you get 20 or less durability on any craftsmen item (axe, pickaxe, shovel, sword, hoe).
 
 ## Installation
 1. Simply drop in /plugins folder of your bukkit/spigot installation
--- src/de/paskl/durability/PlayerListener.java
+++ src/de/paskl/durability/PlayerListener.java
@@ -18,42 +18,50 @@ public class PlayerListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority= EventPriority.HIGH)
-    public void onInteract(PlayerInteractEvent event){
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onInteract(PlayerInteractEvent event) {
         Player p = event.getPlayer();
         ItemStack heldItem = p.getInventory().getItemInMainHand();
 
         Material m = heldItem.getType();
 
         //TODO beautify this
-        if(m == Material.DIAMOND_AXE
-                || m == Material.GOLD_AXE
-                || m == Material.IRON_AXE
-                || m == Material.STONE_AXE
+        if (
+            //Axe
+                m == Material.DIAMOND_AXE
+                        || m == Material.GOLD_AXE
+                        || m == Material.IRON_AXE
+                        || m == Material.STONE_AXE
+                        || m == Material.WOOD_AXE
 
-                || m == Material.DIAMOND_PICKAXE
-                || m == Material.GOLD_PICKAXE
-                || m == Material.IRON_PICKAXE
-                || m == Material.STONE_PICKAXE
-                || m == Material.WOOD_PICKAXE
+                        //Pickaxe
+                        || m == Material.DIAMOND_PICKAXE
+                        || m == Material.GOLD_PICKAXE
+                        || m == Material.IRON_PICKAXE
+                        || m == Material.STONE_PICKAXE
+                        || m == Material.WOOD_PICKAXE
 
-                || m == Material.DIAMOND_HOE
-                || m == Material.GOLD_HOE
-                || m == Material.IRON_HOE
-                || m == Material.STONE_HOE
-                || m == Material.WOOD_HOE
+                        //Hoe
+                        || m == Material.DIAMOND_HOE
+                        || m == Material.GOLD_HOE
+                        || m == Material.IRON_HOE
+                        || m == Material.STONE_HOE
+                        || m == Material.WOOD_HOE
 
-                || m == Material.DIAMOND_SPADE
-                || m == Material.GOLD_SPADE
-                || m == Material.IRON_SPADE
-                || m == Material.STONE_SPADE
-                || m == Material.WOOD_SPADE
+                        //Shade
+                        || m == Material.DIAMOND_SPADE
+                        || m == Material.GOLD_SPADE
+                        || m == Material.IRON_SPADE
+                        || m == Material.STONE_SPADE
+                        || m == Material.WOOD_SPADE
 
-                || m == Material.DIAMOND_SWORD
-                || m == Material.GOLD_SWORD
-                || m == Material.IRON_SWORD
-                || m == Material.STONE_SWORD
-                || m == Material.WOOD_SWORD){
+                        //Sword
+                        || m == Material.DIAMOND_SWORD
+                        || m == Material.GOLD_SWORD
+                        || m == Material.IRON_SWORD
+                        || m == Material.STONE_SWORD
+                        || m == Material.WOOD_SWORD
+                ) {
 
 
             double max = heldItem.getType().getMaxDurability();
@@ -61,9 +69,9 @@ public class PlayerListener implements Listener {
             int leftDurability = ((int) max) - ((int) uses);
 
             //TODO make configurable
-            if(uses > 0 && 20.0 >= max - uses) {
+            if (uses > 0 && 20.0 >= max - uses) {
                 p.sendMessage(ChatColor.RED + "Item has too little duration!\nLeft Duration: " + leftDurability);
-                //event.setCancelled(true);
+                //event.setCancelled(true); //If you want to cancel the event. Can be bad in combat, so its disabled.
             }/* else { //TODO ONLY FOR DEBUGGING
                 Integer integerObject = new Integer("20");
                 short dura = (short)(max - integerObject.shortValue());
